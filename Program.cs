e77874c02ab197eb59e87d659b5bf81d017df16e
duongtam0412 <34595077+duongtam0412@users.noreply.github.com>
2019-05-20 09:09:32 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AnalyzeImageInStreamAsync
+{
+    class Program
+    {   // subscriptionKey = "tamduong"
+        private const string subscriptionKey = "tamduong";
+
+        // localImagePath = @"C:\Documents\LocalImage.jpg"
+        private const string localImagePath = @"E:\bodystyle\New folder (2)\image.PNG";
+
+        private const string remoteImageUrl =
+            "https://pngimage.net/wp-content/uploads/2018/06/png-imagenes-1.png";
+
+        // Specify the features to return
+        private static readonly List<VisualFeatureTypes> features =
+            new List<VisualFeatureTypes>()
+        {
+            VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+            VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+            VisualFeatureTypes.Tags
+        };
+        static void Main(string[] args)
+        {
+            ComputerVisionClient computerVision = new ComputerVisionClient(
+                  new ApiKeyServiceClientCredentials(subscriptionKey),
+                  new System.Net.Http.DelegatingHandler[] { });
+
+            // You must use the same region as you used to get your subscription
+            // keys. For example, if you got your subscription keys from westus,
+            // replace "westcentralus" with "westus".
+            //
+            // Free trial subscription keys are generated in the "westus"
+            // region. If you use a free trial subscription key, you shouldn't
+            // need to change the region.
+
+            // Specify the Azure region
+            computerVision.Endpoint = "https://tamduong.cognitiveservices.azure.com/vision/v2.0/analyze?visualFeatures=ImageType&details=Landmarks&language=en";
+
+            Console.WriteLine("Images being analyzed ...");
+            var t1 = AnalyzeRemoteAsync(computerVision, remoteImageUrl);
+            var t2 = AnalyzeLocalAsync(computerVision, localImagePath);
+
+            Task.WhenAll(t1, t2).Wait(5);
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+        }
+
+        // Analyze a remote image
+        private static async Task AnalyzeRemoteAsync(
+            ComputerVisionClient computerVision, string imageUrl)
+        {
+            if (!Uri.IsWellFormedUriString(imageUrl, UriKind.Absolute))
+            {
+                Console.WriteLine(
+                    "\nInvalid remoteImageUrl:\n{0} \n", imageUrl);
+                return;
+            }
+
+            ImageAnalysis analysis =
+                await computerVision.AnalyzeImageAsync(imageUrl, features);
+            DisplayResults(analysis, imageUrl);
+        }
+
+        // Analyze a local image
+        private static async Task AnalyzeLocalAsync(
+            ComputerVisionClient computerVision, string imagePath)
+        {
+            if (!File.Exists(imagePath))
+            {
+                Console.WriteLine(
+                    "\nUnable to open or read localImagePath:\n{0} \n", imagePath);
+                return;
+            }
+
+            using (Stream imageStream = File.OpenRead(imagePath))
+            {
+                ImageAnalysis analysis = await computerVision.AnalyzeImageInStreamAsync(
+                    imageStream, features);
+                DisplayResults(analysis, imagePath);
+            }
+        }
+
+        // Display the most relevant caption for the image
+        private static void DisplayResults(ImageAnalysis analysis, string imageUri)
+        {
+            Console.WriteLine(imageUri);
+            if (analysis.Description.Captions.Count != 0)
+            {
+                Console.WriteLine(analysis.Description.Captions[0].Text + "\n");
+            }
+            else
+            {
+                Console.WriteLine("No description generated.");
+            }
+        }
+    }
+}
